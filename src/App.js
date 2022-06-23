7b96279e1e40ce5ea7b20bc99c62c1c59f80520a
calgil <clgilbert1105@gmail.com>
2022-06-23 09:44:17 -0600

    heading text edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import money from './assets/money-mountain.jpeg';
 function App() {
   return (
     <div className="App">
-      <h1>Debt Free Calculator</h1>
+      <h1>Debt Payment Calculator</h1>
         <DebtFreeCalculator />
         <div className='money-container'>
           <img className='money' src={money} alt={'moneyMountain'} />
